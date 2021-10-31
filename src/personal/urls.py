a3dcfe9f8c0dd258fca968086c9f4d64b82d40b1
apustovitin <apustovitin@gmail.com>
2021-10-31 17:39:23 +0300

    last_commit

--- src/personal/urls.py
+++ src/personal/urls.py
@@ -5,5 +5,5 @@ from personal.views import home_screen_view, chats_screen_view
 
 urlpatterns = [
     path('', home_screen_view, name='home'),
-    re_path(r'(?P<room_id>\d{1,3})/$', chats_screen_view, name='chats'),
+    re_path(r'^(?P<room_id>\d{1,3})/$', chats_screen_view, name='chats'),
 ]
